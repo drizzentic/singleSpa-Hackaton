6485caed676fc2bb848bf4608c8a450b3617a7ab
Fatma Ali <tumz92@gmail.com>
2019-04-10 17:08:11 +0300

    patientdashboard

--- patientDashboard/src/app/app.module.ts
+++ patientDashboard/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { PatientService } from './patient.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { PocHttpInteceptor } from './http-interceptor';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,17 @@ import { APP_BASE_HREF } from '@angular/common';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [{provide: APP_BASE_HREF, useValue: '/patientDashboard'}],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/patientDashboard'},
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: PocHttpInteceptor,
+      multi: true
+    },
+    PatientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
